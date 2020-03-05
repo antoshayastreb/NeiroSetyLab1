8729de396dcc76ebc354d4a08c9aa07b21f9abe2
Нариман Намазов <nedanon@outlook.com>
2020-03-05 21:09:58 +0300

    Добавьте файлы проекта.

---
+++ NeiroSetyLab1/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NeiroSetyLab1
+{
+    public partial class Form1 : Form
+    {
+        smp sm = new smp();
+
+        Color[] colors = new Color[] {Color.Brown, Color.Crimson, Color.HotPink, Color.DarkOrange, Color.LimeGreen,
+        Color.Red, Color.Green, Color.BurlyWood, Color.Ivory, Color.CadetBlue, Color.Khaki, Color.Fuchsia, Color.MediumBlue,
+        Color.DarkViolet, Color.LightBlue, Color.Gainsboro, Color.Magenta, Color.Aquamarine, Color.AliceBlue, Color.Coral,
+        Color.Cornsilk};
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            pictureBox1.Image = (Image)new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics g = Graphics.FromImage(pictureBox1.Image);
+            Pen p = new Pen(Color.LightGreen);
+            for (int i = 0; i < 9; i++)
+            {
+                g.DrawLine(p, new Point((pictureBox1.Width / 10 * (i + 1)), 0), new Point((pictureBox1.Width / 10 * (i + 1)), pictureBox1.Height));
+                g.DrawLine(p, new Point(0, (pictureBox1.Height / 10 * (i + 1))), new Point(pictureBox1.Width, (pictureBox1.Height / 10 * (i + 1))));
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int X = pictureBox1.Width / 10;
+            int Y = pictureBox1.Height / 10;
+
+            sm.T = Convert.ToInt32(textBox1.Text);
+            pictureBox1.Image = (Image)new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics g = Graphics.FromImage(pictureBox1.Image);
+            Pen p = new Pen(Color.LightGreen);
+            for (int i = 0; i < 9; i++)
+            {
+                g.DrawLine(p, new Point((pictureBox1.Width / 10 * (i + 1)), 0), new Point((pictureBox1.Width / 10 * (i + 1)), pictureBox1.Height));
+                g.DrawLine(p, new Point(0, (pictureBox1.Height / 10 * (i + 1))), new Point(pictureBox1.Width, (pictureBox1.Height / 10 * (i + 1))));
+            }
+            sm.compute();
+
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 0; j < 20; j++)
+                {
+                    p.Color = colors[i];
+                    p.Width = 8.0F;
+
+                    if (sm.klaster[j] - 1 == i)
+                    {
+                        g.DrawEllipse(p, sm.xx[j] * X - 1, sm.xy[j] * Y - 1, 3, 3);
+                    }
+                }
+    
+            }
+
+            listBox1.Items.Clear();
+            for (int i = 0; i< sm.zi; i++)
+            {
+                listBox1.Items.Add("Центр" + (i+1) +" = "+ sm.zx[i]+ ';' + sm.zy[i]);
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sm.setRand();
+            int X = pictureBox1.Width / 10;
+            int Y = pictureBox1.Height / 10;
+
+
+            pictureBox1.Image = (Image)new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics g = Graphics.FromImage(pictureBox1.Image);
+            Pen p = new Pen(Color.LightGreen);
+            for (int i = 0; i < 9; i++)
+            {
+                g.DrawLine(p, new Point((pictureBox1.Width / 10 * (i + 1)), 0), new Point((pictureBox1.Width / 10 * (i + 1)), pictureBox1.Height));
+                g.DrawLine(p, new Point(0, (pictureBox1.Height / 10 * (i + 1))), new Point(pictureBox1.Width, (pictureBox1.Height / 10 * (i + 1))));
+            }
+
+            p.Color = Color.Black;
+            p.Width = 8.0F;
+
+            for (int i = 0; i < 20; i++)
+            {
+                g.DrawEllipse(p, sm.xx[i]*X-1, sm.xy[i]*Y-1, 3, 3);
+
+            }
+
+            
+
+        }
+
+
+        
+    }
+}
---
+++ NeiroSetyLab1/smp.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeiroSetyLab1
+{
+    class smp
+    {
+        //static int n { get; set; }
+
+        static int n = 20;
+
+        public int[] xx = new int[n];
+
+        public int[] xy = new int[n];
+
+        public int[] zx = new int[n];
+
+        public int[] zy = new int[n];
+
+        public int[] klaster = new int[n];
+
+        public double[,] d = new double[n, n];
+
+        public int zi = 0;
+        
+        public int xi = 1;
+        
+        public int k = 0;
+
+        public int T { get; set; }
+
+        public void setRand()
+        {
+            Random rand = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                xx[i] = rand.Next(9);
+                xy[i] = rand.Next(9);
+            }
+        }
+
+        public void compute()
+        {
+            zx[0] = xx[0];
+            zy[0] = xy[0];
+
+            klaster[0] = 1;
+
+            
+
+            while (xi < n)
+            {
+                double mind = 10.0;
+
+                for (int i = 0; i <= zi; i++)
+                {
+                    d[i, xi] = Math.Sqrt(Math.Pow(xx[xi] - zx[i], 2) + Math.Pow(xy[xi] - zy[i], 2));
+
+                    if (d[i, xi] < mind)
+                    {
+                        mind = d[i, xi];
+                        k = i;
+                    }
+                }
+
+                if (mind < T)
+                {
+                    klaster[xi] = k + 1;
+                    xi++;
+                }
+                else
+                {
+                    zi++;
+                    zx[zi] = xx[xi];
+                    zy[zi] = xy[xi];
+                }
+            }
+        }
+    }
+}
